4a7efaf190301d21b4768f7bc3cfd74fe70e2f22
weber0522bb <weber0522bb@gmail.com>
2017-12-13 14:57:47 +0800

    Add files via upload

--- visualize.py
+++ visualize.py
@@ -1,3 +1,4 @@
+
 """
 Mask R-CNN
 Display and Visualization Functions.
@@ -75,7 +76,7 @@ def apply_mask(image, mask, color, alpha=0.5):
 
 def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
-                      figsize=(16, 16), ax=None):
+                      figsize=(16, 16), fig=None):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [num_instances, height, width]
@@ -91,18 +92,17 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     else:
         assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
 
-    if not ax:
-        _, ax = plt.subplots(1, figsize=figsize)
+    if not fig:
+        fig = plt.figure(figsize=figsize)
 
     # Generate random colors
     colors = random_colors(N)
 
     # Show area outside image boundaries.
     height, width = image.shape[:2]
-    ax.set_ylim(height + 10, -10)
-    ax.set_xlim(-10, width + 10)
-    ax.axis('off')
-    ax.set_title(title)
+    fig.add_axes(-10, -10, width+20, height+20)
+    #fig.axis('off')
+    fig.title(title)
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
@@ -116,7 +116,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
                               alpha=0.7, linestyle="dashed",
                               edgecolor=color, facecolor='none')
-        ax.add_patch(p)
+        fig.add_patch(p)
 
         # Label
         class_id = class_ids[i]
@@ -124,7 +124,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         label = class_names[class_id]
         x = random.randint(x1, (x1 + x2) // 2)
         caption = "{} {:.3f}".format(label, score) if score else label
-        ax.text(x1, y1 + 8, caption,
+        fig.text(x1, y1 + 8, caption,
                 color='w', size=11, backgroundcolor="none")
 
         # Mask
@@ -141,8 +141,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             # Subtract the padding and flip (y, x) to (x, y)
             verts = np.fliplr(verts) - 1
             p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
-    ax.imshow(masked_image.astype(np.uint8))
+            fig.add_patch(p)
+    fig.imshow(masked_image.astype(np.uint8))
 
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
