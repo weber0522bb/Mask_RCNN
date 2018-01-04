55a124d1163c9079cfd92bfe85e9a7a5574f60d7
weber0522bb <weber0522bb@gmail.com>
2018-01-04 10:22:28 +0800

    modified:   dbexpos_image.png 	modified:   demo.py 	modified:   mask.png 	modified:   plot.png

--- demo.py
+++ demo.py
@@ -65,8 +65,8 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
 
 # Load a random image from the images folder
 file_names = next(os.walk(IMAGE_DIR))[2]
-image = skimage.io.imread(os.path.join(IMAGE_DIR, 'IMG_0039.JPG'))
-background_image = skimage.io.imread(os.path.join(IMAGE_DIR,'IMG_0057.JPG'))/255
+image = skimage.io.imread(os.path.join(IMAGE_DIR, 'IMG_0040.JPG'))
+background_image = skimage.io.imread(os.path.join(IMAGE_DIR,'IMG_0058.JPG'))/255
 # Run detection
 results = model.detect([image], verbose=1)
 
