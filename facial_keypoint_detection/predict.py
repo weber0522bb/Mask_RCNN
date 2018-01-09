b367a3d3285ae0f4c52c8801b3c53ea1755ab73b
JunTao-Zhang <31920550+JunTao-Zhang@users.noreply.github.com>
2018-01-09 20:00:41 +0800

    Update predict.py

--- facial_keypoint_detection/predict.py
+++ facial_keypoint_detection/predict.py
@@ -22,10 +22,10 @@ def read_img(img):
     '''
     '''
     print ('img0:',img)
-    img = scipy.misc.toimage(img)
     '''
+    img = scipy.misc.toimage(img)
     print ('img1:',img, np.size(img))
-    img = Image.fromarray(img.astype(np.uint64), 'RGB')
+    img = Image.fromarray(img, 'RGB')
     print ('img2:',img)
     L = img.convert('L')
     print ('L:',L)
