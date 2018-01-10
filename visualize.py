65cde57501510f1b0d55431ffa3ffd22eec96e31
weber0522bb <weber0522bb@gmail.com>
2018-01-10 16:51:11 +0800

    modified:   background_filter.png 	modified:   dbexpos_image.png 	modified:   plot.png 	modified:   visualize.py

--- visualize.py
+++ visualize.py
@@ -173,8 +173,8 @@ def display_instances(image, background, i, boxes, masks, class_ids, class_names
     x_product = b[1]/96
     min_x ,min_y,max_x,max_y = bboxes[:4]
     print(bboxes,"bboxes")
-    Leye_x = np.floor(y_pred[6]*x_product+min_x)
-    Leye_y = np.floor((96-y_pred[7])*y_product+min_y)
+    Leye_x = np.floor(y_pred[0]*x_product+min_x)
+    Leye_y = np.floor((96-y_pred[1])*y_product+min_y)
     nose_x = np.ceil(y_pred[20]*x_product+min_x)
     nose_y = np.ceil((96-y_pred[21])*y_product+min_y)
     mouse_x = np.ceil(y_pred[26]*x_product+min_x)
@@ -222,8 +222,8 @@ def nosefilter(img,Leye_x,Leye_y,nose_x,nose_y,mouse_x,mouse_y):
         sine_sita = a/c
         center_x = float(nose_x)
         center_y = float(nose_y)
-        radius_x = 2.5*scale*abs(float(Leye_x)-center_x)
-        radius_y = 2.5*scale*abs(float(Leye_y)-center_y)
+        radius_x = 3*scale*abs(float(Leye_x)-center_x)
+        radius_y = 3*scale*abs(float(Leye_y)-center_y)
         print(center_x,center_y,radius_x,radius_y)
         min_a = center_x - radius_x
         if min_a < 0 :
