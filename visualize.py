6f3c1758cfddd09757d669415ce1bcf1e79a1927
weber0522bb <weber0522bb@gmail.com>
2018-01-10 15:59:42 +0800

    modified:   background_filter.png 	modified:   dbexpos_image.png 	modified:   plot.png 	modified:   visualize.py

--- visualize.py
+++ visualize.py
@@ -172,8 +172,8 @@ def display_instances(image, background, i, boxes, masks, class_ids, class_names
     y_product = b[0]/60
     x_product = b[1]/60
     min_x ,min_y,max_x,max_y = bboxes[:4]
-    Leye_x = np.floor(y_pred[6]*x_product+min_x)
-    Leye_y = np.floor((96-y_pred[7])*y_product+min_y)
+    Leye_x = np.floor(y_pred[4]*x_product+min_x)
+    Leye_y = np.floor((96-y_pred[5])*y_product+min_y)
     nose_x = np.ceil(y_pred[20]*x_product+min_x)
     nose_y = np.ceil((96-y_pred[21])*y_product+min_y)
     mouse_x = np.ceil(y_pred[26]*x_product+min_x)
